63cf298cf3fa5cd4b42249c5017dd69d301f0566
SejoMont <sejo.monterocastro@gmail.com>
2023-10-17 18:48:33 +0200

    uwu

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import Contador from "./components/Contador";
+import DibujosComplejos from "./components/DibujosComplejos";
+import DibujosComplejosReact from "./components/DibujosComplejosReact";
+import PadreDeportes from "./components/PadreDeportes";
+import HijoDeporte from "./components/HijoDeporte";
+import SumaPadre from "./components/SumaPadre";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+    {/* <Contador inicio="12" />
+    <Contador inicio="14" />
+    <Contador inicio="28" /> */}
+    {/* <DibujosComplejos/> */}
+    {/* <DibujosComplejosReact/> */}
+    {/* <PadreDeportes/> */}
+    <SumaPadre/>
+
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
